ea5b6f6143d92e3989038ed560f782be50e73e27
TatySCV <tatiana.campos03@inacapmail.cl>
2026-06-13 00:42:19 -0400

    Cambio a HashRouter para GitHub Pages

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import RegisterPage from "../pages/RegisterPage";
 import ReceiptPage from "../pages/ReceiptPage";
 
 function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RegisterPage />} />
         <Route path="/receipt" element={<ReceiptPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
